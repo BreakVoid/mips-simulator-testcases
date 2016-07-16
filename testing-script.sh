7894cb159b9e9eb467b190ba8070a8bd721c601e
BreakVoid <breakvoid@hotmail.com>
2016-07-16 23:13:21 +0800

    fix a bug in testing-script.sh

--- testing-script.sh
+++ testing-script.sh
@@ -70,7 +70,6 @@ for name in ${names[@]}; do
         filein=${filec%.s}.in
         fileout=${filec%.s}.ans
         _SC cp $filec data.s
-        echo "TESTING " + $(filec)
         if [ ! -f $filein ]; then 
             timeout $timeo $MSCK data.s 1>msckout 2>/dev/null
         else
